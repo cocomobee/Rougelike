ab6b17a5ce017a5b71483df9892bf5d8819477fe
cocomobee <cocomobee@gmail.com>
2025-02-01 09:03:46 +0800

    Fixing up things in main to make it work

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tcod
 
 
-def main():
+def main() -> None:
     screen_width = 80
     screen_height = 50
 
     tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        "Rougelike/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     with tcod.context.new_terminal(
@@ -16,7 +16,7 @@ def main():
         title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
-        root_console = tcod.console(screen_width, screen_height, order="F")
+        root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             root_console.print(x=1, y=1, string="@")
 
