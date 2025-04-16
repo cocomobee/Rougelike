f706f9efb5138bad3a70110586e5e9cc4fba93bc
cocomobee <cocomobee@gmail.com>
2025-04-16 09:56:36 +0800

    Part8 refactors

--- game_map.py
+++ game_map.py
@@ -28,6 +28,11 @@ class GameMap:
             )  # Tiles the player has seen before
         
     @property
+    def gamemap(self) -> GameMap:
+        """Return this map."""
+        return self
+        
+    @property
     def actors(self) -> Iterator[Actor]:
         """Iterate over this maps living actors."""
         yield from (
