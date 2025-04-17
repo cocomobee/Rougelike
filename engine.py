7b78c8b045e22d68899ef505f7ff1c1798aae814
cocomobee <cocomobee@gmail.com>
2025-04-17 14:08:38 +0800

    Part 9 Done

--- engine.py
+++ engine.py
@@ -1,26 +1,26 @@
 from __future__ import annotations
 
+import lzma
+import pickle
 from typing import TYPE_CHECKING
 
 from tcod.console import Console
 from tcod.map import compute_fov
 
 import exceptions
-from input_handlers import MainGameEventHandler
 from message_log import MessageLog
 from render_functions import render_bar, render_names_at_mouse_location
 from game_map import GameMap
 
 if TYPE_CHECKING:
     from entity import Actor
-    from input_handlers import EventHandler
+    from game_map import GameMap
 
 
 class Engine:
     game_map = GameMap
 
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.message_log = MessageLog()
         self.mouse_location = (0, 0)
         self.player = player
@@ -56,3 +56,9 @@ class Engine:
         )
 
         render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
